61bebfd02e68bd156651db36d888335069bc0689
Renato Filogenio <Renato.filogenio@gmail.com>
2025-05-23 17:22:00 -0300

    ajustando cors

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,22 +1,31 @@
-import express, {Request,Response,NextFunction} from "express"
-import 'express-async-errors'
+import express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors';
 import cors from 'cors';
-import {router} from './routes'
+import { router } from './routes';
+
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 app.use(router);
-app.use(()=>{
-    app.use((err: Error, req:Request,res:Response)=>{
-        if(err instanceof Error){
-          return res.status(400).json({
-            error: err.message
-          })
-        }
-        return res.status(500).json({
-            status:'error',
-            message:'Erro interno'
-        })
-    })
-})
-app.listen(3333,()=> console.log("Server online"))
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Erro interno'
+  });
+});
+
+app.listen(3333, '127.0.0.1', () => console.log("Server online"));
